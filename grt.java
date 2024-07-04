f8a74f3cbf317ec29253b1ade56566edbc3cb398
Varsha Gupta <vrsa8896@gmail.com>
2024-07-04 15:51:28 +0530

    ..

---
+++ grt.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class grt{
+    public static void main(String arg[]){
+        int arr[] = { 5, 4, 3,2, 0, 0, 2};
+        int arr2[] = new int[arr.length];
+        int smlnum = 0;
+        // for(int i = 0; i<arr.length; i++ ){
+        //     if(arr[i] > grtn){
+        //         grtn = arr[i];
+        //     }
+        // }
+        // System.out.println(grtn);
+       
+       
+        int x =0;
+            for(int i = 0; i<arr.length; i++){
+               
+                for(int j = i+1; j<arr2.length; i++){
+                    
+                }
+                
+               
+            }
+            
+            
+    }
+}
+
+       
